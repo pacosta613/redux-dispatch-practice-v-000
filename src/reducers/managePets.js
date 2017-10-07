9e880596dca9d58f9e1f7cfbec2bf55065f46d82
Pedro Acosta <pedro.acosta613@gmail.com>
2017-10-07 23:05:15 +0000

    automatically backed up by learn

--- src/reducers/managePets.js
+++ src/reducers/managePets.js
@@ -6,6 +6,8 @@ export function managePets(state = { pets: [] }, action){
   switch (action.type) {
     case "ADD_PET":
       return { pets: [...state.pets, action.pet] };
+    case "REMOVE_PET":
+      return { pets: state.pets.filter( pet => pet.id !== action.id) }
     default:
       return state;
   }
