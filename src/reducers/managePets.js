e917407a8dcc039c37129fe8c84b5183cce794da
Pedro Acosta <pedro.acosta613@gmail.com>
2017-10-07 23:06:39 +0000

    automatically backed up by learn

--- src/reducers/managePets.js
+++ src/reducers/managePets.js
@@ -16,8 +16,9 @@ export function managePets(state = { pets: [] }, action){
 
 export function dispatch(action){
 
-    
-  
+    state = managePets(state, action);
+    render()
+
 }
 
 export function render(){
